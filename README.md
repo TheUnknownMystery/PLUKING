75a26417c2b81e0c6bfe44e19acb01262fa32879
sauhardo2020 <70318077+sauhardo2020@users.noreply.github.com>
2020-09-30 10:36:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PLUKING
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ stone.js
@@ -0,0 +1,58 @@
+class stoneObj{
+
+constructor(x,y,radius){
+
+    var options={
+
+isStatic: false,
+restitution: 0,
+friction: 1,
+density: 1.2
+
+
+                }
+this.body= Bodies.rectangle(x,y,radius,options)
+
+this.radius= radius;
+
+this.image=loadImage("stone.png")
+World.add(world,this.body)
+
+}
+
+display(){
+
+var pos= this.body.position;
+var radius= this.body.radius;
+image(this.image,pos.x,pos.y,radius)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
